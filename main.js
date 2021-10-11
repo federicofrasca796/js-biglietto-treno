09aefe7d70acdc33d94dcab15487d534f6ee9681
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-11 15:04:19 +0200

    add ask info and return gross price

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+//Ask train user how many kilometers is his commute and his age.
+const commuteKm = prompt("How many kilometers is your commute?");
+
+//Ask train user his age.
+const userAge = prompt("How old are you?");
+
+//Assume price per km = 0.21€/km. 
+const pricePerKm = 0.21;
+//Calculate commute gross price.
+const grossPrice = pricePerKm * commuteKm;
+console.log(`${grossPrice}€`);
+//Discount 20% off for under 18 users and 40% for over 65 users.
+
+//Output must be rounded to 2 decimal places maximum.
