9efe2668f7f44d8a7830a65a8b76e09f69b331db
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-11 16:18:06 +0200

    return ticket price inside html|complete excercise

--- main.js
+++ main.js
@@ -11,23 +11,23 @@ const grossPrice = pricePerKm * commuteKm;
 
 //Discount 20% off for under 18 users and 40% for over 65 users.
 //Output must be rounded to 2 decimal places maximum.
-console.log('Your ticket will cost:')
-
 if (userAge < 18) {
 
     const discount_20 = grossPrice - (grossPrice * 20/100);    
     //Round decimals to a maximum of 2:
     console.log(Math.round(discount_20 * 100) / 100 + '€');
-
+    document.getElementById('ticket_price').innerHTML = `Your ticket will cost ${Math.round(discount_20 * 100) / 100 + '€'}`;
+    
 } else if (userAge > 65) {
 
     const discount_40 = grossPrice - (grossPrice * 40/100);    
     //Round decimals to a maximum of 2:
     console.log(Math.round(discount_40 * 100) / 100 + '€');
+    document.getElementById('ticket_price').innerHTML = `Your ticket will cost ${Math.round(discount_40 * 100) / 100 + '€'}`;
 
 } else {    
     //Round decimals to a maximum of 2:
     console.log(Math.round(grossPrice * 100) / 100 + '€');
+    document.getElementById('ticket_price').innerHTML = `Your ticket will cost ${Math.round(grossPrice * 100) / 100 + '€'}`;
 }
-
 
