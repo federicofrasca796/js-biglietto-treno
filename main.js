4f54876a5b30975f156fcc5ffeafd12193c35ba7
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-11 15:33:22 +0200

    add conditions to apply discount

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Ask train user how many kilometers is his commute and his age.
-const commuteKm = prompt("How many kilometers is your commute?");
+const commuteKm = parseFloat(prompt("How many kilometers is your commute?"));
 
 //Ask train user his age.
 const userAge = prompt("How old are you?");
@@ -8,7 +8,24 @@ const userAge = prompt("How old are you?");
 const pricePerKm = 0.21;
 //Calculate commute gross price.
 const grossPrice = pricePerKm * commuteKm;
-console.log(`${grossPrice}€`);
-//Discount 20% off for under 18 users and 40% for over 65 users.
 
+//Discount 20% off for under 18 users and 40% for over 65 users.
+let discount_20;
+let discount_40;
+
+if (userAge < 18) {
+
+    discount_20 = grossPrice - (grossPrice * 20/100);
+    console.log(`${discount_20}€`);
+
+} else if  (userAge > 65) {
+
+    discount_40 = grossPrice - (grossPrice * 40/100);
+    console.log(`${discount_40}€`);
+
+} else {
+
+    console.log(`${grossPrice}€`);
+    
+}
 //Output must be rounded to 2 decimal places maximum.
