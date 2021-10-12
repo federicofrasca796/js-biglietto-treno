d620f159488c5bf82ce7c5956d01bba0b5c18309
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-12 10:43:58 +0200

    add isNaN check

--- main.js
+++ main.js
@@ -7,17 +7,25 @@ const userAge = parseInt(prompt("How old are you?"));
 //Assume price per km = 0.21€/km. Calculate commute gross price.
 let price = .21 * commuteKm;
 
+//add check if user typed numbers
+if (isNaN(userAge) || isNaN(commuteKm)){
+    console.log("You need to type numbers.")
+    document.getElementById('ticket_price').innerHTML = `You need to type numbers.`;
+}
+
 //Discount 20% off for under 18 users and 40% for over 65 users.
 //Output must be rounded to 2 decimal places maximum.
 if (userAge < 18) {
 
     price -= price * 0.2;   
-    
+    //Round decimals to a maximum of 2:
+    document.getElementById('ticket_price').innerHTML = `Your ticket will cost ${Math.round(price * 100) / 100 + '€'}`;
+
 } else if (userAge > 65) {
 
     price -= price * 0.4; 
-
+    //Round decimals to a maximum of 2:
+    document.getElementById('ticket_price').innerHTML = `Your ticket will cost ${Math.round(price * 100) / 100 + '€'}`;
+    
 }
 
-//Round decimals to a maximum of 2:
-document.getElementById('ticket_price').innerHTML = `Your ticket will cost ${Math.round(price * 100) / 100 + '€'}`;
