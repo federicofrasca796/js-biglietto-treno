bc2343ec6997abf2a2251e59360dc32f81275274
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-12 10:32:59 +0200

    optimize code after live coding

--- main.js
+++ main.js
@@ -1,33 +1,23 @@
 //Ask train user how many kilometers is his commute and his age.
-const commuteKm = parseFloat(prompt("How many kilometers is your commute?"));
+const commuteKm = parseInt(prompt("How many kilometers is your commute?"));
 
 //Ask train user his age.
-const userAge = prompt("How old are you?");
+const userAge = parseInt(prompt("How old are you?"));
 
-//Assume price per km = 0.21€/km. 
-const pricePerKm = 0.21;
-//Calculate commute gross price.
-const grossPrice = pricePerKm * commuteKm;
+//Assume price per km = 0.21€/km. Calculate commute gross price.
+let price = .21 * commuteKm;
 
 //Discount 20% off for under 18 users and 40% for over 65 users.
 //Output must be rounded to 2 decimal places maximum.
 if (userAge < 18) {
 
-    const discount_20 = grossPrice - (grossPrice * 20/100);    
-    //Round decimals to a maximum of 2:
-    console.log(Math.round(discount_20 * 100) / 100 + '€');
-    document.getElementById('ticket_price').innerHTML = `Your ticket will cost ${Math.round(discount_20 * 100) / 100 + '€'}`;
+    price -= price * 0.2;   
     
 } else if (userAge > 65) {
 
-    const discount_40 = grossPrice - (grossPrice * 40/100);    
-    //Round decimals to a maximum of 2:
-    console.log(Math.round(discount_40 * 100) / 100 + '€');
-    document.getElementById('ticket_price').innerHTML = `Your ticket will cost ${Math.round(discount_40 * 100) / 100 + '€'}`;
+    price -= price * 0.4; 
 
-} else {    
-    //Round decimals to a maximum of 2:
-    console.log(Math.round(grossPrice * 100) / 100 + '€');
-    document.getElementById('ticket_price').innerHTML = `Your ticket will cost ${Math.round(grossPrice * 100) / 100 + '€'}`;
 }
 
+//Round decimals to a maximum of 2:
+document.getElementById('ticket_price').innerHTML = `Your ticket will cost ${Math.round(price * 100) / 100 + '€'}`;
